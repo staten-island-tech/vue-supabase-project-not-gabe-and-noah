4cd3020c17c51df682d08b24ef6b577764932db4
noaha90 <noahabb@outlook.com>
2024-04-15 09:50:14 -0400

    ewq
    weqwqqwe

---
+++ vite-project/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import Account from './components/Account.vue'
+import Auth from './components/Auth.vue'
+import { supabase } from './supabase'
+
+const session = ref()
+
+onMounted(() => {
+  supabase.auth.getSession().then(({ data }) => {
+    session.value = data.session
+  })
+
+  supabase.auth.onAuthStateChange((_, _session) => {
+    session.value = _session
+  })
+},
+  console.log(session)
+)
+</script>
+
+<template>
+  <div class="container" style="padding: 50px 0 100px 0">
+    <Account v-if="session" :session="session" />
+    <Auth v-else />
+  </div>
+</template>
