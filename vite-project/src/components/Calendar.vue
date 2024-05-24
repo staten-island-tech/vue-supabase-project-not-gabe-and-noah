ff27b06bb0e1d5f38ff9134761f5bfaa6582e799
rylan-geykhman <144476581+rylan-geykhman@users.noreply.github.com>
2024-05-24 13:12:41 -0400

    wer
    wer

---
+++ vite-project/src/components/Calendar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+<p>test</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+helloIzzy(test);
+
+function helloIzzy(fn: (a: string) => void) {
+  fn("Hello, World");
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vite-project/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+<Calendar></Calendar>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Calendar from '@/components/Calendar.vue';
+</script>
+
+<style scoped>
+
+</style>
