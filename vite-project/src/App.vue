8d36716dfb31a99032b318088948a48db468ab80
Noahabbas80 <121987786+Noahabbas80@users.noreply.github.com>
2024-05-28 20:01:30 -0400

    eqw
    eqw

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
 import Login from '@/components/Login.vue';
 </script>
 
 <template>
+  <NavBar></NavBar>
   <header>
       <nav>
         <Login />
