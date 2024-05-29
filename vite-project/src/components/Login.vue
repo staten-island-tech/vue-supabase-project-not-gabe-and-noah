26cbcffa2ef75bd3e3e94b46e6855ebbeb4b7b01
Noahabbas80 <121987786+Noahabbas80@users.noreply.github.com>
2024-05-28 20:14:34 -0400

    qwe
    eqw

--- vite-project/src/components/Login.vue
+++ vite-project/src/components/Login.vue
@@ -155,11 +155,15 @@ input {
 	border-radius: 10px;
   	box-shadow: 0 14px 28px rgba(0,0,0,0.25), 
 			0 10px 10px rgba(0,0,0,0.22);
-	position: relative;
+	position: absolute;
+	top: 50%;
+	left: 50%;
 	overflow: hidden;
 	width: 768px;
 	max-width: 100%;
 	min-height: 480px;
+	transform: translate(-50%, -50%);
+
 }
 
 .form-container {
