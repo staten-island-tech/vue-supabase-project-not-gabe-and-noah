8e057a4ecfde7b3187aac8742dc2148cc61b4331
Noahabbas80 <121987786+Noahabbas80@users.noreply.github.com>
2024-05-30 12:48:42 -0400

    eqw
    wqe

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -2,17 +2,18 @@
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from '@/components/NavBar.vue'
 import Login from '@/components/Login.vue';
+let scene = 4
 import main from '@/components/main.vue';
 </script>
 
 <template>
 
 
-
   <header>
     <NavBar></NavBar>
       <nav>
-       <main></main>
+        <Login v-if="scene == 3"></Login>
+       <main v-if="scene == 4"></main>
       </nav>
   </header>
 </template>
