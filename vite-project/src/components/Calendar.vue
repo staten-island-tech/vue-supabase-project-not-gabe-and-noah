b7a0745b95dd16955fa8d82254fc93d2c86d3e50
Noahabbas80 <121987786+Noahabbas80@users.noreply.github.com>
2024-06-06 14:18:36 -0400

    qew
    qwe

---
+++ vite-project/src/components/Calendar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+<p>wilds</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
--- vite-project/src/views/Main.vue
+++ vite-project/src/views/Main.vue
@@ -1,37 +1,16 @@
 <template>
-  <Transition>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-  </Transition>
+<calendar></calendar>
 </template>
 
 <style>
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
 
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    background-color: goldenrod;
-  }
-}
 </style>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import { info } from "@/stores/store"
 import { useRouter } from 'vue-router'
+import calendar from "@/components/Calendar.vue"
 const router = useRouter()
 const store = info(); 
 
